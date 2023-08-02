0c842b4e4eb9ad68815e53f181776784f722659f
Alex Simisi Nta <alexsms.devops@gmail.com>
2023-08-02 15:16:05 +0200

    create index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const authMiddleware = require("./middleware/authMiddleware");
+const login = require("./api/auth/login");
+const register = require("./api/auth/register");
+const profile = require("./api/user/profile");
+const colors = require("colors");
+const cors = require("cors");
+const helmet = require("helmet");
+require("dotenv").config();
+
+
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log(`Connected to MongoDB`.bgYellow.italic.bold))
+  .catch((error) => console.error("Error connecting to MongoDB: ", error));
+
+const app = express()
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/auth", login)
+app.use("/api/auth", register)
+app.use("/api/user", authMiddleware, profile)
+
+module.exports = app;
